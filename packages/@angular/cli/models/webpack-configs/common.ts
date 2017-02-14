019d589ef76b27060394f161e6017ee8a9a35c30
rdm0509 <rmendoza@austin.utexas.edu>
2017-02-14 14:43:00 -0600

    fixed conflicts; installed publicPath options

--- packages/@angular/cli/models/webpack-configs/common.ts
+++ packages/@angular/cli/models/webpack-configs/common.ts
@@ -1,10 +1,10 @@
 import * as webpack from 'webpack';
 import * as path from 'path';
-import { GlobCopyWebpackPlugin } from '../../plugins/glob-copy-webpack-plugin';
-import { packageChunkSort } from '../../utilities/package-chunk-sort';
-import { BaseHrefWebpackPlugin } from '../../lib/base-href-webpack';
-import { extraEntryParser, lazyChunksFilter, getOutputHashFormat } from './utils';
-import { WebpackConfigOptions } from '../webpack-config';
+import {GlobCopyWebpackPlugin} from '../../plugins/glob-copy-webpack-plugin';
+import {packageChunkSort} from '../../utilities/package-chunk-sort';
+import {BaseHrefWebpackPlugin} from '../../lib/base-href-webpack';
+import {extraEntryParser, lazyChunksFilter, getOutputHashFormat} from './utils';
+import {WebpackConfigOptions} from '../webpack-config';
 
 const autoprefixer = require('autoprefixer');
 const ProgressPlugin = require('webpack/lib/ProgressPlugin');
@@ -24,140 +24,131 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
  */
 
 export function getCommonConfig(wco: WebpackConfigOptions) {
-  const { projectRoot, buildOptions, appConfig } = wco;
+    const {projectRoot, buildOptions, appConfig} = wco;
 
-  const appRoot = path.resolve(projectRoot, appConfig.root);
-  const nodeModules = path.resolve(projectRoot, 'node_modules');
+    const appRoot = path.resolve(projectRoot, appConfig.root);
+    const nodeModules = path.resolve(projectRoot, 'node_modules');
 
-  let extraPlugins: any[] = [];
-  let extraRules: any[] = [];
-  let entryPoints: { [key: string]: string[] } = {};
+    let extraPlugins: any[] = [];
+    let extraRules: any[] = [];
+    let entryPoints: {[key: string]: string[]} = {};
 
-  // figure out which are the lazy loaded entry points
-  const lazyChunks = lazyChunksFilter([
-    ...extraEntryParser(appConfig.scripts, appRoot, 'scripts'),
-    ...extraEntryParser(appConfig.styles, appRoot, 'styles')
-  ]);
+    // figure out which are the lazy loaded entry points
+    const lazyChunks = lazyChunksFilter([
+        ...extraEntryParser(appConfig.scripts, appRoot, 'scripts'),
+        ...extraEntryParser(appConfig.styles, appRoot, 'styles')
+    ]);
 
-  if (appConfig.main) {
-    entryPoints['main'] = [path.resolve(appRoot, appConfig.main)];
-  }
-
-  if (appConfig.polyfills) {
-    entryPoints['polyfills'] = [path.resolve(appRoot, appConfig.polyfills)];
-  }
-
-  // determine hashing format
-  const hashFormat = getOutputHashFormat(buildOptions.outputHashing);
-
-  // process global scripts
-  if (appConfig.scripts.length > 0) {
-    const globalScripts = extraEntryParser(appConfig.scripts, appRoot, 'scripts');
-
-    // add entry points and lazy chunks
-    globalScripts.forEach(script => {
-      let scriptPath = `script-loader!${script.path}`;
-      if (script.lazy) { lazyChunks.push(script.entry); }
-      entryPoints[script.entry] = (entryPoints[script.entry] || []).concat(scriptPath);
-    });
-  }
-
-  if (buildOptions.vendorChunk) {
-    extraPlugins.push(new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      chunks: ['main'],
-      minChunks: (module: any) => module.resource && module.resource.startsWith(nodeModules)
-    }));
-  }
-
-  // process asset entries
-  if (appConfig.assets) {
-    extraPlugins.push(new GlobCopyWebpackPlugin({
-      patterns: appConfig.assets,
-      globOptions: { cwd: appRoot, dot: true, ignore: '**/.gitkeep' }
-    }));
-  }
-
-  if (buildOptions.progress) {
-    extraPlugins.push(new ProgressPlugin({ profile: buildOptions.verbose, colors: true }));
-  }
-
-  let publicPaths = appConfig.publicPaths || {};
-
-  let publicPath = publicPaths[environment] || appConfig.deployUrl || '';
-
-  return {
-    devtool: buildOptions.sourcemap ? 'source-map' : false,
-    resolve: {
-      extensions: ['.ts', '.js'],
-      modules: [nodeModules],
-    },
-    resolveLoader: {
-      modules: [nodeModules]
-    },
-    context: projectRoot,
-    entry: entryPoints,
-    output: {
-<<<<<<< HEAD:packages/angular-cli/models/webpack-build-common.ts
-      path: path.resolve(projectRoot, appConfig.outDir),
-      publicPath: publicPath,
-      filename: `[name].bundle.js`,
-      sourceMapFilename: `[name].bundle.map`,
-      chunkFilename: `[id].chunk.js`,
-=======
-      path: path.resolve(projectRoot, buildOptions.outputPath),
-      publicPath: buildOptions.deployUrl,
-      filename: `[name]${hashFormat.chunk}.bundle.js`,
-      sourceMapFilename: `[name]${hashFormat.chunk}.bundle.map`,
-      chunkFilename: `[id]${hashFormat.chunk}.chunk.js`
->>>>>>> e55cb8237a8af66e1fe59b62a90d86aa3c717990:packages/@angular/cli/models/webpack-configs/common.ts
-    },
-    module: {
-      rules: [
-        { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader', exclude: [nodeModules] },
-        { test: /\.json$/, loader: 'json-loader' },
-        { test: /\.html$/, loader: 'raw-loader' },
-        { test: /\.(eot|svg)$/, loader: `file-loader?name=[name]${hashFormat.file}.[ext]` },
-        {
-          test: /\.(jpg|png|gif|otf|ttf|woff|woff2|cur|ani)$/,
-          loader: `url-loader?name=[name]${hashFormat.file}.[ext]&limit=10000`
-        }
-      ].concat(extraRules)
-    },
-    plugins: [
-<<<<<<< HEAD:packages/angular-cli/models/webpack-build-common.ts
-      new ExtractTextPlugin({
-        filename: `[name].bundle.css`,
-        // disable: !extractCss
-      }),
-=======
-      new webpack.NoEmitOnErrorsPlugin(),
->>>>>>> e55cb8237a8af66e1fe59b62a90d86aa3c717990:packages/@angular/cli/models/webpack-configs/common.ts
-      new HtmlWebpackPlugin({
-        template: path.resolve(appRoot, appConfig.index),
-        filename: path.resolve(buildOptions.outputPath, appConfig.index),
-        chunksSortMode: packageChunkSort(appConfig),
-        excludeChunks: lazyChunks,
-        xhtml: true
-      }),
-      new BaseHrefWebpackPlugin({
-        baseHref: buildOptions.baseHref
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        minChunks: Infinity,
-        name: 'inline'
-      })
-    ].concat(extraPlugins),
-    node: {
-      fs: 'empty',
-      global: true,
-      crypto: 'empty',
-      tls: 'empty',
-      net: 'empty',
-      process: true,
-      module: false,
-      clearImmediate: false,
-      setImmediate: false
+    if (appConfig.main) {
+        entryPoints['main'] = [path.resolve(appRoot, appConfig.main)];
     }
-  };
+
+    if (appConfig.polyfills) {
+        entryPoints['polyfills'] = [path.resolve(appRoot, appConfig.polyfills)];
+    }
+
+    // determine hashing format
+    const hashFormat = getOutputHashFormat(buildOptions.outputHashing);
+
+    // process global scripts
+    if (appConfig.scripts.length > 0) {
+        const globalScripts = extraEntryParser(appConfig.scripts, appRoot, 'scripts');
+
+        // add entry points and lazy chunks
+        globalScripts.forEach(script => {
+            let scriptPath = `script-loader!${script.path}`;
+            if (script.lazy) {
+                lazyChunks.push(script.entry);
+            }
+            entryPoints[script.entry] = (entryPoints[script.entry] || []).concat(scriptPath);
+        });
+    }
+
+    if (buildOptions.vendorChunk) {
+        extraPlugins.push(new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            chunks: ['main'],
+            minChunks: (module: any) => module.resource && module.resource.startsWith(nodeModules)
+        }));
+    }
+
+    // process asset entries
+    if (appConfig.assets) {
+        extraPlugins.push(new GlobCopyWebpackPlugin({
+            patterns: appConfig.assets,
+            globOptions: {cwd: appRoot, dot: true, ignore: '**/.gitkeep'}
+        }));
+    }
+
+    if (buildOptions.progress) {
+        extraPlugins.push(new ProgressPlugin({profile: buildOptions.verbose, colors: true}));
+    }
+
+    let publicPaths = appConfig.publicPaths || {};
+
+    let publicPath = publicPaths[buildOptions.environment] || buildOptions.deployUrl || '';
+
+    return {
+        devtool: buildOptions.sourcemap ? 'source-map' : false,
+        resolve: {
+            extensions: ['.ts', '.js'],
+            modules: [nodeModules],
+        },
+        resolveLoader: {
+            modules: [nodeModules]
+        },
+        context: projectRoot,
+        entry: entryPoints,
+        output: {
+            path: path.resolve(projectRoot, appConfig.outDir),
+            publicPath: publicPath,
+            filename: `[name].bundle.js`,
+            sourceMapFilename: `[name].bundle.map`,
+            chunkFilename: `[id].chunk.js`,
+        },
+        module: {
+            rules: [
+                {enforce: 'pre', test: /\.js$/, loader: 'source-map-loader', exclude: [nodeModules]},
+                {test: /\.json$/, loader: 'json-loader'},
+                {test: /\.html$/, loader: 'raw-loader'},
+                {test: /\.(eot|svg)$/, loader: `file-loader?name=[name]${hashFormat.file}.[ext]`},
+                {
+                    test: /\.(jpg|png|gif|otf|ttf|woff|woff2|cur|ani)$/,
+                    loader: `url-loader?name=[name]${hashFormat.file}.[ext]&limit=10000`
+                }
+            ].concat(extraRules)
+        },
+        plugins: [
+            // new ExtractTextPlugin({
+            //   filename: `[name].bundle.css`,
+            //   // disable: !extractCss
+            // }),
+            new webpack.NoEmitOnErrorsPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(appRoot, appConfig.index),
+                filename: path.resolve(buildOptions.outputPath, appConfig.index),
+                chunksSortMode: packageChunkSort(appConfig),
+                excludeChunks: lazyChunks,
+                xhtml: true
+            }),
+            new BaseHrefWebpackPlugin({
+                baseHref: buildOptions.baseHref
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                minChunks: Infinity,
+                name: 'inline'
+            })
+        ].concat(extraPlugins),
+        node: {
+            fs: 'empty',
+            global: true,
+            crypto: 'empty',
+            tls: 'empty',
+            net: 'empty',
+            process: true,
+            module: false,
+            clearImmediate: false,
+            setImmediate: false
+        }
+    };
 }
--- packages/@angular/cli/models/webpack-configs/styles.ts
+++ packages/@angular/cli/models/webpack-configs/styles.ts
@@ -1,10 +1,10 @@
 import * as webpack from 'webpack';
 import * as path from 'path';
 import {
-  SuppressExtractedTextChunksWebpackPlugin
+    SuppressExtractedTextChunksWebpackPlugin
 } from '../../plugins/suppress-entry-chunks-webpack-plugin';
-import { extraEntryParser, getOutputHashFormat } from './utils';
-import { WebpackConfigOptions } from '../webpack-config';
+import {extraEntryParser, getOutputHashFormat} from './utils';
+import {WebpackConfigOptions} from '../webpack-config';
 
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
@@ -27,123 +27,130 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
  */
 
 export function getStylesConfig(wco: WebpackConfigOptions) {
-  const { projectRoot, buildOptions, appConfig } = wco;
+    const {projectRoot, buildOptions, appConfig} = wco;
 
-  const appRoot = path.resolve(projectRoot, appConfig.root);
-  const entryPoints: { [key: string]: string[] } = {};
-  const globalStylePaths: string[] = [];
-  const extraPlugins: any[] = [];
-  // style-loader does not support sourcemaps without absolute publicPath, so it's
-  // better to disable them when not extracting css
-  // https://github.com/webpack-contrib/style-loader#recommended-configuration
-  const cssSourceMap = buildOptions.extractCss && buildOptions.sourcemap;
+    const appRoot = path.resolve(projectRoot, appConfig.root);
+    const entryPoints: {[key: string]: string[]} = {};
+    const globalStylePaths: string[] = [];
+    const extraPlugins: any[] = [];
+    // style-loader does not support sourcemaps without absolute publicPath, so it's
+    // better to disable them when not extracting css
+    // https://github.com/webpack-contrib/style-loader#recommended-configuration
+    const cssSourceMap = buildOptions.extractCss && buildOptions.sourcemap;
 
-  // minify/optimize css in production
-  // autoprefixer is always run separately so disable here
-  const extraPostCssPlugins = buildOptions.target === 'production'
-    ? [cssnano({ safe: true, autoprefixer: false })]
-    : [];
+    // minify/optimize css in production
+    // autoprefixer is always run separately so disable here
+    const extraPostCssPlugins = buildOptions.target === 'production'
+        ? [cssnano({safe: true, autoprefixer: false})]
+        : [];
 
-  // determine hashing format
-  const hashFormat = getOutputHashFormat(buildOptions.outputHashing);
+    // determine hashing format
+    const hashFormat = getOutputHashFormat(buildOptions.outputHashing);
 
-  // use includePaths from appConfig
-  const includePaths: string[] = [];
+    // use includePaths from appConfig
+    const includePaths: string[] = [];
 
-  if (appConfig.stylePreprocessorOptions
-    && appConfig.stylePreprocessorOptions.includePaths
-    && appConfig.stylePreprocessorOptions.includePaths.length > 0
-  ) {
-    appConfig.stylePreprocessorOptions.includePaths.forEach((includePath: string) =>
-      includePaths.push(path.resolve(appRoot, includePath)));
-  }
-
-  // process global styles
-  if (appConfig.styles.length > 0) {
-    const globalStyles = extraEntryParser(appConfig.styles, appRoot, 'styles');
-    // add style entry points
-    globalStyles.forEach(style =>
-      entryPoints[style.entry]
-        ? entryPoints[style.entry].push(style.path)
-        : entryPoints[style.entry] = [style.path]
-    );
-    // add global css paths
-    globalStylePaths.push(...globalStyles.map((style) => style.path));
-  }
-
-  // set base rules to derive final rules from
-  const baseRules = [
-    { test: /\.css$/, loaders: [] },
-    { test: /\.scss$|\.sass$/, loaders: ['sass-loader'] },
-    { test: /\.less$/, loaders: ['less-loader'] },
-    // stylus-loader doesn't support webpack.LoaderOptionsPlugin properly,
-    // so we need to add options in its query
-    {
-      test: /\.styl$/, loaders: [`stylus-loader?${JSON.stringify({
-        sourceMap: cssSourceMap,
-        paths: includePaths
-      })}`]
+    if (appConfig.stylePreprocessorOptions
+        && appConfig.stylePreprocessorOptions.includePaths
+        && appConfig.stylePreprocessorOptions.includePaths.length > 0
+    ) {
+        appConfig.stylePreprocessorOptions.includePaths.forEach((includePath: string) =>
+            includePaths.push(path.resolve(appRoot, includePath)));
     }
-  ];
 
-  const commonLoaders = [
-    // css-loader doesn't support webpack.LoaderOptionsPlugin properly,
-    // so we need to add options in its query
-    `css-loader?${JSON.stringify({ sourceMap: cssSourceMap, importLoaders: 1 })}`,
-    'postcss-loader'
-  ];
+    // process global styles
+    if (appConfig.styles.length > 0) {
+        const globalStyles = extraEntryParser(appConfig.styles, appRoot, 'styles');
+        // add style entry points
+        globalStyles.forEach(style =>
+            entryPoints[style.entry]
+                ? entryPoints[style.entry].push(style.path)
+                : entryPoints[style.entry] = [style.path]
+        );
+        // add global css paths
+        globalStylePaths.push(...globalStyles.map((style) => style.path));
+    }
 
-  // load component css as raw strings
-  let rules: any = baseRules.map(({test, loaders}) => ({
-    exclude: globalStylePaths, test, loaders: [
-      'exports-loader?module.exports.toString()',
-      ...commonLoaders,
-      ...loaders
-    ]
-  }));
+    // set base rules to derive final rules from
+    const baseRules = [
+        {test: /\.css$/, loaders: []},
+        {test: /\.scss$|\.sass$/, loaders: ['sass-loader']},
+        {test: /\.less$/, loaders: ['less-loader']},
+        // stylus-loader doesn't support webpack.LoaderOptionsPlugin properly,
+        // so we need to add options in its query
+        {
+            test: /\.styl$/, loaders: [`stylus-loader?${JSON.stringify({
+            sourceMap: cssSourceMap,
+            paths: includePaths
+        })}`]
+        }
+    ];
 
-  // load global css as css files
-  if (globalStylePaths.length > 0) {
-    rules.push(...baseRules.map(({test, loaders}) => ({
-      include: globalStylePaths, test, loaders: ExtractTextPlugin.extract({
-        use: [
-          ...commonLoaders,
-          ...loaders
-        ],
-        fallback: 'style-loader',
-        // publicPath needed as a workaround https://github.com/angular/angular-cli/issues/4035
-        publicPath: ''
-      })
-    })));
-  }
+    const commonLoaders = [
+        // css-loader doesn't support webpack.LoaderOptionsPlugin properly,
+        // so we need to add options in its query
+        `css-loader?${JSON.stringify({sourceMap: cssSourceMap, importLoaders: 1})}`,
+        'postcss-loader'
+    ];
 
-  // supress empty .js files in css only entry points
-  if (buildOptions.extractCss) {
-    extraPlugins.push(new SuppressExtractedTextChunksWebpackPlugin());
-  }
+    console.log('commonLoaders', commonLoaders);
 
-  return {
-    entry: entryPoints,
-    module: { rules },
-    plugins: [
-      // extract global css from js files into own css file
-      new ExtractTextPlugin({
-        filename: `[name]${hashFormat.extract}.bundle.css`,
-        disable: !buildOptions.extractCss
-      }),
-      new webpack.LoaderOptionsPlugin({
-        sourceMap: cssSourceMap,
-        options: {
-          postcss: [autoprefixer()].concat(extraPostCssPlugins),
-          // css-loader, stylus-loader don't support LoaderOptionsPlugin properly
-          // options are in query instead
-          sassLoader: { sourceMap: cssSourceMap, includePaths },
-          // less-loader doesn't support paths
-          lessLoader: { sourceMap: cssSourceMap },
-          // context needed as a workaround https://github.com/jtangelder/sass-loader/issues/285
-          context: projectRoot,
-        },
-      })
-    ].concat(extraPlugins)
-  };
+    // load component css as raw strings
+    let rules: any = baseRules.map(({test, loaders}) => ({
+        exclude: globalStylePaths, test, loaders: [
+            'exports-loader?module.exports.toString()',
+            ...commonLoaders,
+            ...loaders
+        ]
+    }));
+
+    let publicPaths = appConfig.publicPaths || {};
+
+    let publicPath = publicPaths[buildOptions.environment] || buildOptions.deployUrl || '';
+
+    // load global css as css files
+    if (globalStylePaths.length > 0) {
+        console.log('wtf', baseRules);
+        rules.push(...baseRules.map(({test, loaders}) => ({
+            include: globalStylePaths, test, loaders: ExtractTextPlugin.extract({
+                use: [
+                    ...commonLoaders,
+                    ...loaders
+                ],
+                fallback: 'style-loader',
+                // publicPath needed as a workaround https://github.com/angular/angular-cli/issues/4035
+                publicPath: publicPath
+            })
+        })));
+    }
+
+    // supress empty .js files in css only entry points
+    if (buildOptions.extractCss) {
+        extraPlugins.push(new SuppressExtractedTextChunksWebpackPlugin());
+    }
+
+    return {
+        entry: entryPoints,
+        module: {rules},
+        plugins: [
+            // extract global css from js files into own css file
+            new ExtractTextPlugin({
+                filename: `[name].bundle.css`,
+                disable: !buildOptions.extractCss
+            }),
+            new webpack.LoaderOptionsPlugin({
+                sourceMap: cssSourceMap,
+                options: {
+                    postcss: [autoprefixer()].concat(extraPostCssPlugins),
+                    // css-loader, stylus-loader don't support LoaderOptionsPlugin properly
+                    // options are in query instead
+                    sassLoader: {sourceMap: cssSourceMap, includePaths},
+                    // less-loader doesn't support paths
+                    lessLoader: {sourceMap: cssSourceMap},
+                    // context needed as a workaround https://github.com/jtangelder/sass-loader/issues/285
+                    context: projectRoot,
+                },
+            })
+        ].concat(extraPlugins)
+    };
 }
